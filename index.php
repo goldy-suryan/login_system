2899f467b10c8923736a786b6ecadf6c0dbaa3f3
Goldy <goldysuryan87@gmail.com>
2017-12-01 17:19:03 +0530

    carousel for the home page

--- index.php
+++ index.php
@@ -1,7 +1,52 @@
 <?php
 include "header.php";
 ?>
+  <div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+      </ol>
 
-<?php
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="images/image1.jpg" alt="image1">
+        </div>
+
+        <div class="item">
+          <img src="images/image2.jpg" alt="image2">
+        </div>
+
+        <div class="item">
+          <img src="images/image3.jpg" alt="image3">
+        </div>
+
+        <div class="item">
+          <img src="images/image4.jpg" alt="image4">
+        </div>
+
+        <div class="item">
+          <img src="images/image5.jpg" alt="image5">
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+  <?php
 include "footer.php";
 ?>
