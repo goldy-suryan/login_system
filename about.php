25e70890283d62ed0932c90936929e69e8771ca0
Goldy <goldysuryan87@gmail.com>
2017-12-02 19:54:17 +0530

    about page

---
+++ about.php
@@ -0,0 +1,103 @@
+<?php
+include_once "header.php";
+?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Vivamuslibero Auguer</h2>
+        <p>In sed neque id libero pretium luctus. Vivamus faucibus. Ut vitae elit. In hac habitasse platea dictumst. Proin et nisl ac orci tempus luctus. Aenean lacinia justo at nisi. Vestibulum sed eros sit amet nisl lobortis commodo. Suspendisse nulla.
+          Vivamus ac lorem. Aliquam pulvinar purus at felis. Quisque convallis nulla id ipsum. Praesent vitae urna. Fusce blandit nunc nec mi. Praesent vestibulum hendrerit ante.</p>
+        <p>Vivamus accumsan. Donec molestie pede vel urna. Curabitur eget sem ornare felis gravida vestibulum.Sed pulvinar, tellus in venenatis vehicula, lorem magna dignissim erat, in accumsan ante lorem sit amet lorem.</p>
+      </div>
+      <div class="col-md-6">
+        <img src="https://placeholdit.co//i/470x250?bg=111111" alt="placeholder" class="img-responsive">
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Vivamuslibero Auguer</h2>
+        <p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Et harumd und.</p>
+        <p>Excepteur sint occaecat cupidatat non proident.</p>
+      </div>
+      <div class="col-md-4">
+        <h2>Vivamuslibero Auguer</h2>
+        <div class="row section">
+          <div class="col-md-3">
+            <img src="https://placeholdit.co//i/64x64?bg=111111" alt="placeholder">
+          </div>
+          <div class="col-md-9">
+            <h4 id="column">Indonectetus facilis</h4>
+            <p>Inteligula congue id elis donec sce sag ittis intes id laoreet aenean.</p>
+          </div>
+        </div>
+        <div class="row section">
+          <div class="col-md-3">
+            <img src="https://placeholdit.co//i/64x64?bg=111111" alt="placeholder">
+          </div>
+          <div class="col-md-9">
+            <h4 id="column">Indonectetus facilis</h4>
+            <p>Inteligula congue id elis donec sce sag ittis intes id laoreet aenean.</p>
+          </div>
+        </div>
+        <div class="row section">
+          <div class="col-md-3">
+            <img src="https://placeholdit.co//i/64x64?bg=111111" alt="placeholder">
+          </div>
+          <div class="col-md-9">
+            <h4 id="column">Indonectetus facilis</h4>
+            <p>Inteligula congue id elis donec sce sag ittis intes id laoreet aenean.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h2>Indonectetus facilis</h2>
+        <p>Aliquam venenatis leo et orci.</p>
+        <p>Pellentesque eleifend vulputate massa..</p>
+        <p>Vivamus eleifend sollicitudin eros.</p>
+        <p>Maecenas vitae nunc.</p>
+        <p>Ut pretium odio eu nisi.</p>
+        <p>Nam condimentum mi id magna.</p>
+        <p>Pellentesque consectetuer, felis vel rhoncus.</p>
+        <p>Sed sollicitudin bibendum dui.</p>
+      </div>
+    </div>
+    <hr>
+
+    <div class="row">
+      <div class="col-md-4">
+        <img src="https://placeholdit.co//i/360x220?bg=111111" alt="placeholder" class="img-responsive">
+        <div class="content">
+          <h4>Name goes here</h4>
+          <p><small>Job title here</small></p>
+          <hr>
+          <p>Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+        </div>
+      </div>
+      <div class="col-md-4 inner">
+        <img src="https://placeholdit.co//i/360x220?bg=111111" alt="placeholder" class="img-responsive">
+        <div class="content">
+          <h4>Name goes here</h4>
+          <p><small>Job title here</small></p>
+          <hr>
+          <p>Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+        </div>
+      </div>
+      <div class="col-md-4 inner">
+        <img src="https://placeholdit.co//i/360x220?bg=111111" alt="placeholder" class="img-responsive">
+        <div class="content">
+          <h4>Name goes here</h4>
+          <p><small>Job title here</small></p>
+          <hr>
+          <p>Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+include "footer.php";
+?>
